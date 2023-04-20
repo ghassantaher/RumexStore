cbec35a6cae35e5ba3898c34246e4a177e7fa585
ghassantaher <ghassan.taher@hotmail.com>
2023-04-20 13:33:54 -0400

    added jest unit tests

--- rumexstore-react/src/App.test.js
+++ rumexstore-react/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, cleanup, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /ATTENTION: This Website is for training/i,
+  );
   expect(linkElement).toBeInTheDocument();
+});
+
+afterEach(cleanup);
+
+test('When App first rendered, loading indicator should show', async () => {
+  const { container } = render(<App />);
+  const loading = container.getElementsByClassName('lds-spinner');
+  expect(loading).not.toBeNull();
 });
