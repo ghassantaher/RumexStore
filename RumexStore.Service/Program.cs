b8b18759765d3e47e642f634935bc9724851922d
ghassantaher <ghassan.taher@hotmail.com>
2024-07-17 16:31:10 -0400

    heartbeat-get

--- RumexStore.Service/Program.cs
+++ RumexStore.Service/Program.cs
@@ -56,8 +56,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-app.MapMethods("/api/heartbeat-old", new[] { "HEAD" }, () => Results.Ok());
-app.MapGet("/api/heartbeat", () => Results.Ok());
+app.MapMethods("/api/heartbeat", new[] { "HEAD" }, () => Results.Ok());
+app.MapGet("/api/heartbeat-get", () => Results.Ok());
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
